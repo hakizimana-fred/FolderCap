54a95f951437d5ecf90608f170de5f9efe6bfec1
Hakizimana Fred <hakifred20@gmail.com>
2023-11-02 18:00:01 +0300

    wrap up

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ function getFolderCapacity(dir) {
     return {
       error: "Directory is required",
     };
+
+  if (!fs.existsSync(dir)) {
+    return {
+      error: "Directory does not exist",
+    };
+  }
   let total = 0;
   const files = fs.readdirSync(dir);
-  const folderSizes = {};
 
   for (const file of files) {
     const filePath = path.join(dir, file);
@@ -22,5 +27,33 @@ function getFolderCapacity(dir) {
   }
   return total;
 }
-const output = getFolderCapacity("");
-console.log(output);
+
+function bytesToGB(bytes) {
+  return bytes / 1024 ** 3;
+}
+
+function formatSizeInGB(bytes) {
+  return bytesToGB(bytes).toFixed(2) + "GB";
+}
+
+function getFolderCapacityInfo(dir) {
+  const output = getFolderCapacity(dir);
+  if (output.error) {
+    return {
+      error: output.error,
+    };
+  }
+
+  return {
+    totalBytes: output,
+    totalGB: parseFloat(formatSizeInGB(output)),
+  };
+}
+
+const directoryInfo = getFolderCapacityInfo("");
+
+if (directoryInfo.error) {
+  console.error(directoryInfo.error);
+} else {
+  console.log(`directory info is ${JSON.stringify(directoryInfo)}`);
+}
