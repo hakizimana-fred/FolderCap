4bf6b57c7603229e0feb59f0336c3f0363ba6c70
Hakizimana Fred <hakifred20@gmail.com>
2023-11-02 17:25:09 +0300

    validation

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const fs = require("fs");
 const path = require("path");
 
 function getFolderCapacity(dir) {
+  if (!dir)
+    return {
+      error: "Directory is required",
+    };
+  let total = 0;
   const files = fs.readdirSync(dir);
+  const folderSizes = {};
+
   for (const file of files) {
     const filePath = path.join(dir, file);
     const stats = fs.statSync(filePath);
-    console.log(stats, "just stats");
-  }
-}
 
-getFolderCapacity("/home/ngeni_fred/Development/nodejs");
+    if (stats.isDirectory()) {
+      total += getFolderCapacity(filePath);
+    } else {
+      total += stats.size;
+    }
+  }
+  return total;
+}
+const output = getFolderCapacity("");
+console.log(output);
